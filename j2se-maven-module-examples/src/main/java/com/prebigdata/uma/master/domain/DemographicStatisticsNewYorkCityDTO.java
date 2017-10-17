6afb0b36c70da74fd654bbf86245dada0dcd5e1f
tomy <m.tomas.ortega@gmail.com>
2017-10-17 11:58:00 +0200

    Add demographic data transfer object domain

---
+++ j2se-maven-module-examples/src/main/java/com/prebigdata/uma/master/domain/DemographicStatisticsNewYorkCityDTO.java
@@ -0,0 +1,19 @@
+package com.prebigdata.uma.master.domain;
+
+public class DemographicStatisticsNewYorkCityDTO {
+    public Integer jurisdictionName;
+    public Integer countParticipants;
+    public Integer countFemale;
+    public Double percentFemale;
+    public Integer countMale;
+    public Double percentMale;
+
+    public DemographicStatisticsNewYorkCityDTO() {
+        this.jurisdictionName = null;
+        this.countParticipants = null;
+        this.countFemale = null;
+        this.percentFemale = null;
+        this.countMale = null;
+        this.percentMale = null;
+    }
+}

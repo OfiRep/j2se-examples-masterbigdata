4b4a371d53b41c8a05271fa5654207789bd7b580
tomy <m.tomas.ortega@gmail.com>
2017-10-17 11:26:26 +0200

    Add client data transfer object domain

---
+++ j2se-maven-module-examples/src/main/java/com/prebigdata/uma/master/domain/ClientDTO.java
@@ -0,0 +1,16 @@
+package com.prebigdata.uma.master.domain;
+
+
+public class ClientDTO {
+    public String id;
+    public String name;
+    public String surname;
+    public String city;
+    
+    public ClientDTO() {
+        this.id = null;
+        this.name = null;
+        this.surname = null;
+        this.city = null;
+    }
+}

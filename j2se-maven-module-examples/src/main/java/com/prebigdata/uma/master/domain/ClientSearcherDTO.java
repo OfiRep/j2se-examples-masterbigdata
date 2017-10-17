49b977f97ccdc083bae90798e953c3a1b4e5e358
tomy <m.tomas.ortega@gmail.com>
2017-10-17 11:28:34 +0200

    Add client searcher set

---
+++ j2se-maven-module-examples/src/main/java/com/prebigdata/uma/master/domain/ClientSearcherDTO.java
@@ -0,0 +1,11 @@
+package com.prebigdata.uma.master.domain;
+
+public class ClientSearcherDTO {
+    public String idSearchCriteria;
+    public String nameSearchCriteria;
+    
+    public ClientSearcherDTO() {
+        this.idSearchCriteria = null;
+        this.nameSearchCriteria = null;
+    }
+}
